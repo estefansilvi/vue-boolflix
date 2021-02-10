49bb465cab0dad7bd029672a402615865db350cb
Stefania <estefansilvi@gmail.com>
2021-02-10 15:37:11 +0100

    media voto numero intero

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const punteggio ='voto_average';
 var app = new Vue({
   el: '#search-bar',
   data: {
@@ -6,13 +7,19 @@ var app = new Vue({
   },
   methods:{
 
-    search: function () {
-      const self = this;
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=c040985640a2d910494ec5b91989dd26&query=' + self.searchInput)
-      .then(function(resp) {
-          self.listMovies = resp.data.results;
+  search: function () {
+    const self = this;
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=c040985640a2d910494ec5b91989dd26&query=' + self.searchInput)
+    .then(function(resp) {
+        self.listMovies = resp.data.results;
+         self.votoIntero();
+    });
+  },
+  votoIntero: function() {
+    this.listaMovies.forEach((item,i) => {
+              item[i].vote_average = Math.round(item[i].vote_average / 2)
       });
-    }
+  }
   }
 });
 
