ba5d0a5dd7faf17c2bf7ee37ec9ee313f00b3a14
Stefania <estefansilvi@gmail.com>
2021-02-10 16:08:33 +0100

    stelle al posto del numero  ma son sempre tutte 5 nere

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var app = new Vue({
   el: '#search-bar',
   data: {
     listMovies:[],
-    searchInput:''
+    searchInput:'',
+    voteList:[1,2,3,4,5]
   },
   methods:{
 
@@ -11,15 +12,18 @@ var app = new Vue({
     const self = this;
     axios.get('https://api.themoviedb.org/3/search/movie?api_key=c040985640a2d910494ec5b91989dd26&query=' + self.searchInput)
     .then(function(resp) {
-        self.listMovies = resp.data.results;
-         self.votoIntero();
+      self.listMovies = resp.data.results;
+
     });
   },
-  votoIntero: function() {
-    this.listaMovies.forEach((item,i) => {
-              item[i].vote_average = Math.round(item[i].vote_average / 2)
-      });
-  }
+  coloredStars: function(element, index) {
+    const averageVote = Math.ceil(element.vote_average/2);
+      if (index + 1 <= averageVote) {
+          return 'light-star'
+      } else {
+          return 'white'
+      }
+    }
   }
 });
 
