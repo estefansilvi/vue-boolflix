d097f40b90980bfc48cc8a8d10d573ef9ff1219b
Stefania <estefansilvi@gmail.com>
2021-02-10 22:25:51 +0100

    milestone 2 completa + accenno di css

--- js/script.js
+++ js/script.js
@@ -4,26 +4,35 @@ var app = new Vue({
   data: {
     listMovies:[],
     searchInput:'',
-    voteList:[1,2,3,4,5]
+    voteList:[1,2,3,4,5],
+    arrayFlags:['it','fr','en','es','ja'],
+    serieTvList:[]
+
   },
   methods:{
 
+  /* creo la funzione per la ricerca dei film e delle serie tv */
   search: function () {
     const self = this;
     axios.get('https://api.themoviedb.org/3/search/movie?api_key=c040985640a2d910494ec5b91989dd26&query=' + self.searchInput)
     .then(function(resp) {
       self.listMovies = resp.data.results;
-
+    });
+    axios.get('https://api.themoviedb.org/3/search/tv?api_key=c040985640a2d910494ec5b91989dd26&query=' + self.searchInput)
+    .then(function(resp) {
+        self.serieTvList = resp.data.results;
     });
   },
-  coloredStars: function(element, index) {
-    const averageVote = Math.ceil(element.vote_average/2);
-      if (index + 1 <= averageVote) {
-          return 'light-star'
-      } else {
-          return 'white'
-      }
-    }
+
+  /* creo la funzione per svuotare l'input dopo aver cliccato il bottone o aver premuto enter */
+  resetInput:function(){
+    this.searchInput='';
+  },
+
+  /* creo la funzione per arrotondare ad un numero intero il voto */
+  assegnoVoto: function(voto){
+    return parseInt(voto / 2);
+  },
   }
 });
 
